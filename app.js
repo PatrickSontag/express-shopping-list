ee1852072cdc61a5d4b1e48c5ea79d71e551fa4e
Patrick Sontag <pmsontag84@gmail.com>
2023-01-05 13:58:12 -0600

    logger middleware

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const expressError = require('./expressError');
 const itemRoutes = require("./routes");
+const middleware = require("./middleware");
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(middleware.logger);
+// app.use(express.urlencoded({ extended: true }));
 
 //  apply a prefix to every route in itemRoutes
 app.use("/items", itemRoutes);
@@ -18,10 +20,30 @@ const SHOPPING_LIST = [
 ]
 
 app.get('/', function(req, res) {
-    console.log('root route');
-    console.log(SHOPPING_LIST);
-    return res.send('Shopping List');
+    return res.send("Shopping List");
 });
+
+// // generic error handler
+// app.use(function(err, req, res, next) {
+//     // the default status is 500 Internal Server Error
+//     let status = err.status || 500;
+//     console.log("ERROR 500")
+  
+//     // set the status and alert the user
+//     return res.status(status).json({
+//       error: {
+//         message: err.message,
+//         status: status
+//       }
+//     });
+//   });
+//   // end generic handler
+
+// 404 handler
+app.use(function(req, res) {
+    console.log("404")
+    // return new ExpressError("Not Found", 404);
+  });
 
 app.listen(3000, function() {
     console.log('Server started on port 3000.');
