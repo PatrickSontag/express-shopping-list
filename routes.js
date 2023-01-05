5d76a0d912fe4a37dea94776894af1434ec1a88d
Patrick Sontag <pmsontag84@gmail.com>
2023-01-05 13:57:51 -0600

    add items list

--- routes.js
+++ routes.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const router = new express.Router();
 
-const items = [];
+const items = [
+    { id: 1, item: 'socks', price: 11.95 },
+    { id: 2, item: 'shirt', price: 28.89 },
+    { id: 3, item: 'hat', price: 25.15 }
+];
 
 /** GET /items: get list of items */
 
 router.get("/", function(req, res) {
-    console.log("ITEMS");
-  return res.json(items);
+  return res.json({"items":items});
 });
 
 /** DELETE /items/[id]: delete item, return status */
