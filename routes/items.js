5e7fb7562dcd7fad209d03dbf1c68ab6b4843106
Patrick Sontag <pmsontag84@gmail.com>
2023-01-06 06:45:41 -0600

    hanlde if item not found

--- routes/items.js
+++ routes/items.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = new express.Router();
 const items = require('../fakeDb');
+const ExpressError = require('../expressError');
 
 
 /** GET /items: get list of items */
@@ -12,32 +13,42 @@ router.get("/", function(req, res) {
 
 
 router.post("/", function (req, res) {
-  const newItem = { name: req.body.name, price: req.body.price };
-  items.push(newItem);
-  res.status(201).json({"added": newItem });
+  const foundItem = { name: req.body.name, price: req.body.price };
+  items.push(foundItem);
+  res.status(201).json({"added": foundItem });
 })
 
 router.get("/:name", function(req, res) {
-  const getItem = items.find(item => item.name === req.params.name)
-  return res.json(getItem);
+  const foundItem = items.find(item => item.name === req.params.name);
+  if (foundItem === undefined) {
+    throw new ExpressError("Item not found.", 404)
+  }
+  return res.json(foundItem);
 });
 
 router.patch("/:name", function(req, res) {
   let itemIndex;
   for (let [i, value] of items.entries()) {
     if (value.name === req.params.name) {
-      console.log(i);
       itemIndex = i;
     }
   }
-  const patchItem = items[itemIndex];
-  patchItem.name = req.body.name;
-  patchItem.price = req.body.price;
-  return res.json({"updated":patchItem});
+  if (itemIndex === undefined) {
+    throw new ExpressError("Item not found.", 404)
+  }
+  const foundItem = items[itemIndex];
+  foundItem.name = req.body.name;
+  foundItem.price = req.body.price;
+  return res.json({"updated":foundItem});
 });
 
 router.delete("/:name", function(req, res) {
   const itemIndex = items.findIndex(i => i.name === req.params.name);
+  console.log("itemIndex: ", itemIndex);
+  if (itemIndex === -1) {
+    console.log("delete route error");
+    throw new ExpressError("Item not found.", 404)
+  }
   items.splice(itemIndex, 1);
   return res.json({message: "Deleted"});
 });
