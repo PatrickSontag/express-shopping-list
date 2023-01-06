910664150d605272a924f8d04f62c2c7a27b1d65
Patrick Sontag <pmsontag84@gmail.com>
2023-01-05 22:54:19 -0600

    patch item

--- routes/items.js
+++ routes/items.js
@@ -12,15 +12,28 @@ router.get("/", function(req, res) {
 
 
 router.post("/", function (req, res) {
-    const newItem = { name: req.body.name, price: req.body.price };
-    items.push(newItem);
-    console.log(items);
-    res.status(201).json({"added": newItem });
+  const newItem = { name: req.body.name, price: req.body.price };
+  items.push(newItem);
+  res.status(201).json({"added": newItem });
 })
 
 router.get("/:name", function(req, res) {
   const getItem = items.find(item => item.name === req.params.name)
   return res.json(getItem);
+});
+
+router.patch("/:name", function(req, res) {
+  let itemIndex;
+  for (let [i, value] of items.entries()) {
+    if (value.name === req.params.name) {
+      console.log(i);
+      itemIndex = i;
+    }
+  }
+  const patchItem = items[itemIndex];
+  patchItem.name = req.body.name;
+  patchItem.price = req.body.price;
+  return res.json(patchItem);
 });
 
 /** DELETE /items/[id]: delete item, return status */
