93da404536776c054f9478b6b6fdf0f4d71254d3
Patrick Sontag <pmsontag84@gmail.com>
2023-01-05 22:21:09 -0600

    connect to updated items

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const ExpressError = require('./expressError');
-const itemRoutes = require("./routes");
+const itemRoutes = require("./routes/items");
 const middleware = require("./middleware");
 const items = require('./fakeDb');
 
@@ -8,37 +8,16 @@ const app = express();
 
 app.use(express.json());
 app.use(middleware.logger);
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 //  apply a prefix to every route in itemRoutes
 app.use("/items", itemRoutes);
 // end itemRoutes
 
-// const SHOPPING_LIST = [
-//     { id: 1, item: 'socks', price: 11.95 },
-//     { id: 2, item: 'shirt', price: 28.89 },
-//     { id: 3, item: 'hat', price: 25.15 }
-// ]
-
 app.get('/', function(req, res) {
-    return res.send("Shopping List");
+    return res.json(items);
+    // return res.send("Shopping List");
 });
-
-// // generic error handler
-// app.use(function(err, req, res, next) {
-//     // the default status is 500 Internal Server Error
-//     let status = err.status || 500;
-//     console.log("ERROR 500")
-  
-//     // set the status and alert the user
-//     return res.status(status).json({
-//       error: {
-//         message: err.message,
-//         status: status
-//       }
-//     });
-//   });
-//   // end generic handler
 
 // 404 handler
 app.use(function(req, res) {
