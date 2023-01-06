b1fabeb7fa0f40238f51db8b283d44909bc8bd56
Patrick Sontag <pmsontag84@gmail.com>
2023-01-06 06:54:49 -0600

    add error json response

--- app.js
+++ app.js
@@ -25,6 +25,14 @@ app.use(function(req, res) {
     return new ExpressError("Not Found", 404);
   });
 
+app.use((err, req, res, next) => {
+	res.status(err.status || 500);
+
+	return res.json({
+			error: err.message,
+	});
+});
+
 app.listen(3000, function() {
     console.log('Server started on port 3000.');
 });
